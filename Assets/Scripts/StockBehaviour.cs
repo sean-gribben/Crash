f6103d80a63bb382e35e6b3298430012969696fb
sgri0009 <sgri0009@student.monash.edu>
2021-10-02 14:01:35 +1000

    Initial commit

---
+++ Assets/Scripts/StockBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockBehaviour : MonoBehaviour
+{
+    public string code;
+    public List<float> values = new List<float>();
+    public float volatility;
+    public float rateofchange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
